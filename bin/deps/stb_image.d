5bf3908ebdc211935cc035a3d522148b3c3506c0
RayssaRoseno <rr@ic.ufal.br>
2021-09-27 18:36:04 -0300

    Delete bin directory

--- bin/deps/stb_image.d
+++
@@ -1,2 +0,0 @@
-bin/stb_image.o: src/stb_image.cpp include/stb_image.h
-include/stb_image.h:
--- bin/deps/vec.d
+++
@@ -1,6 +0,0 @@
-bin/vec.o: src/vec.cpp include/pch.h include/GL/glut.h \
- include/GL/freeglut_std.h src/vec.hpp
-include/pch.h:
-include/GL/glut.h:
-include/GL/freeglut_std.h:
-src/vec.hpp:
